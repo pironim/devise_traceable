01b2d7b936faeb9d3c9aef4dd18a0c473d4f1e71
Felipe Mathies <felipe.mathies@gmail.com>
2013-07-09 15:20:33 -0300

    trach on set user

--- lib/devise_traceable/hooks/traceable.rb
+++ lib/devise_traceable/hooks/traceable.rb
@@ -3,6 +3,12 @@
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
 
+Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
+  if record.respond_to?(:stamp!)
+    record.stamp!
+  end
+end
+
 Warden::Manager.before_logout do |record, warden, opts|
   if record.respond_to?(:stamp!)
     record.stamp!
