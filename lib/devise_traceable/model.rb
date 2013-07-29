65a9bc22b99d1848055523e35de10f32e58470d4
Vladimir Zhukov <vzhukov@sphereconsultinginc.com>
2013-07-29 15:30:33 +0000

    path for bfb project to store all info for sti user model

--- lib/devise_traceable/model.rb
+++ lib/devise_traceable/model.rb
@@ -6,11 +6,13 @@ module Devise
 
     # * resource_id
     # * sign_in_at
-    
+
     module Traceable
       def stamp!
-        new_current = Time.now
-        "#{self.class}Tracing".constantize.create(:sign_in_at => self.current_sign_in_at, "#{self.class}".foreign_key.to_sym => self.id)
+        # quick and dirty fix
+        # method modified to store user logins without sti details
+        # assume thats device use only user and inherited classes
+        'UserTracing'.constantize.create(:sign_in_at => Time.now, :user_id => self.id)
       end
     end
   end
